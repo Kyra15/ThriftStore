3a9964cb574bff06967c96702439332bd289eec4
Kyra15 <84361652+Kyra15@users.noreply.github.com>
2024-11-02 00:32:20 -0400

    close to final update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById('min-input').addEventListener('blur', function() {
     if (!isNaN(value)) {
         this.value = value.toFixed(2);
     }
-    validateMinMax();
+    minMaxCheck();
 });
 
 document.getElementById('max-input').addEventListener('blur', function() {
@@ -15,26 +15,36 @@ document.getElementById('max-input').addEventListener('blur', function() {
     if (!isNaN(value)) {
         this.value = value.toFixed(2);
     }
-    validateMinMax();
+    minMaxCheck();
 });
 
-function validateMinMax() {
+function minMaxCheck() {
     const valueMin = parseFloat(document.getElementById('min-input').value);
     const valueMax = parseFloat(document.getElementById('max-input').value);
-    const minWarning = document.getElementById('min-warning');
+    const priceWarning = document.getElementById('price-warning');
 
     if (valueMin > valueMax) {
-        minWarning.style.display = "flex";
+        priceWarning.style.display = "flex";
     } else {
-        minWarning.style.display = "none";
+        priceWarning.style.display = "none";
     }
 }
 
-var selected = [];
+
 var finalArray = [];
+var finalArray1 = [];
+var finalArray2 = [];
+var finalArray3 = [];
+
+var selected = [];
+var selectedSize = [];
+
+filterSelection("all");
+filterSize("all");
+priceSelection();
 
 function filterSelection(c) {
-    if (c === "all") {
+    if (c == "all") {
         selected = [];
     } else {
         const index = selected.indexOf(c);
@@ -43,14 +53,32 @@ function filterSelection(c) {
         } else {
             selected.push(c);
         }
+      }
+
+    var x = document.getElementsByClassName("item-box");
+    finalArray1 = [];
+
+    if (selected.length == 0) {
+        for (let i = 0; i < x.length; i++) {
+            finalArray1.push(x[i]);
+        }
+    } else {
+        for (let i = 0; i < x.length; i++) {
+            if (selected.some(cat => x[i].className.indexOf(cat) > -1)) {
+                finalArray1.push(x[i]);
+            }
+        }
     }
-    applySelectionFilter();
+
+    console.log("Filtered items:", finalArray1);
+    console.log("Selected categories:", selected);
+
+    compareLists();
 }
 
-var selectedSize = [];
 
 function filterSize(c) {
-    if (c === "all") {
+    if (c == "all") {
         selectedSize = [];
     } else {
         const index = selectedSize.indexOf(c);
@@ -59,55 +87,63 @@ function filterSize(c) {
         } else {
             selectedSize.push(c);
         }
-    }
-    applySizeFilter();
-}
+      }
 
-function applySelectionFilter() {
-    const items = document.getElementsByClassName("item-box");
-    finalArray = [];
+    var x = document.getElementsByClassName("item-box");
+    finalArray2 = [];
 
-    for (let i = 0; i < items.length; i++) {
-        if (selected.length === 0 || selected.some(cat => items[i].className.includes(cat))) {
-            finalArray.push(items[i]);
+    if (selectedSize.length == 0) {
+        for (let i = 0; i < x.length; i++) {
+            finalArray2.push(x[i]);
         }
-    }
-    priceSelection();
-}
-
-function applySizeFilter() {
-    const items = document.getElementsByClassName("item-box");
-    const filteredBySize = [];
-
-    for (let i = 0; i < items.length; i++) {
-        if (selectedSize.length === 0 || selectedSize.some(size => items[i].className.includes(size))) {
-            filteredBySize.push(items[i]);
+    } else {
+        for (let i = 0; i < x.length; i++) {
+            if (selectedSize.some(cat => x[i].className.indexOf(cat) > -1)) {
+                finalArray2.push(x[i]);
+            }
         }
     }
 
-    finalArray = finalArray.filter(item => filteredBySize.includes(item));
-    priceSelection();
-}
-
-function priceSelection() {
-    const minPrice = parseFloat(document.getElementById('min-input').value) || 0;
-    const maxPrice = parseFloat(document.getElementById('max-input').value) || Infinity;
-    const items = document.getElementsByClassName('item-box');
-
-    finalArray = finalArray.filter(item => {
-        const priceText = item.getElementsByClassName('item-price')[0].textContent;
-        const itemPrice = parseFloat(priceText.replace('$', ''));
-        return itemPrice >= minPrice && itemPrice <= maxPrice;
-    });
+    console.log("Filtered items 2:", finalArray2);
+    console.log("Selected categories 2:", selectedSize);
 
     compareLists();
 }
 
+
+function priceSelection() {
+    var min_price = parseFloat(document.getElementById('min-input').value) || 0;
+    var max_price = parseFloat(document.getElementById('max-input').value) || Infinity;
+    let items = document.getElementsByClassName('item-box');
+
+    finalArray3 = [];
+    for (i = 0; i < items.length; i++) {
+        let priceText = items[i].getElementsByClassName('item-price')[0].textContent;
+        let it_price = parseFloat(priceText.replace('$', ''));
+
+        if (it_price <= max_price && it_price >= min_price) {
+            if (!finalArray3.includes(items[i])) {
+                finalArray3.push(items[i]);
+            }
+        }
+    }
+
+    console.log("Filtered items 3:", finalArray3);
+
+    compareLists();
+}
+
+
 function compareLists() {
-    const uniqueArray = Array.from(new Set(finalArray));
+    finalArray = finalArray1.filter(value => finalArray2.includes(value) && finalArray3.includes(value));
+    console.log("finalarray", finalArray);
+//    https://stackoverflow.com/questions/19395257/how-to-count-duplicate-value-in-an-array-in-javascript
+    const counts = {};
+    finalArray.forEach(function(i) { counts[i] = (counts[i]||0) + 1;});
 
     clearItems();
-    uniqueArray.forEach(item => AddCat(item, "show-item"));
+
+    finalArray.forEach(item => AddCat(item, "show-item"));
 }
 
 function clearItems() {
@@ -118,24 +154,30 @@ function clearItems() {
 }
 
 function AddCat(element, name) {
-    const arr1 = element.className.split(" ");
-    const arr2 = name.split(" ");
-    arr2.forEach(cat => {
-        if (!arr1.includes(cat)) {
-            element.className += " " + cat;
-        }
-    });
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
 }
 
 function RemoveCat(element, name) {
-    let arr1 = element.className.split(" ");
-    name.split(" ").forEach(cat => {
-        arr1 = arr1.filter(item => item !== cat);
-    });
-    element.className = arr1.join(" ");
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
 }
 
-// Uncomment if needed
+
+// Uncomment to get it to search while typing i js dont rly need it rn
 // document.getElementById('searchbar').addEventListener('input', lookUp);
 function lookUp() {
     let input = document.getElementById('searchbar').value.toLowerCase();
